35a785c409df71b7f6ac12656250f57aa82d329d
vishal-04-singh <vishal-04-singh@users.noreply.github.com>
2025-05-27 23:29:23 +0530

    complete first project

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const fs = require('fs');
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'public')));
-  
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     fs.readdir('./hisaab', (err, files) => {
         if (err) return res.status(500).send(err);
@@ -23,13 +23,46 @@ app.get('/create', (req, res) => {
     res.render('create');
 });
 
-app.post('/createhisaab', (req, res) => {
+
+app.get('/hisaab/:filename', (req, res) => {
+    fs.readFile(`./hisaab/${req.params.filename}`, 'utf8', (err, filedata) => {
+        if (err) return res.status(500).send(err);
+        res.render('hisaab', { filedata, filename: req.params.filename });
+    });
+});
+app.get('/delete/:filename', (req, res) => {
+    fs.unlink(`./hisaab/${req.params.filename}`, (err) => {
+        if (err) return res.status(500).send(err);
+        res.redirect('/');
     
+});
+
+
+});
+
+
+app.get('/edit/:filename', (req, res) => {
+    fs.readFile(`./hisaab/${req.params.filename}`, 'utf8', (err, filedata) => {
+        if (err) return res.status(500).send(err);
+        res.render("edit", { filedata, filename: req.params.filename });
+    });
+});
+
+
+app.post('/update/:filename', (req, res) => {
+    fs.writeFile(`./hisaab/${req.params.filename}`, req.body.content, (err) => {
+        if (err) return res.status(500).send
+        res.redirect('/');
+    });
+});
+
+app.post('/createhisaab', (req, res) => {
+
     var currentDate = new Date();
     var date = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`;
     fs.writeFile(`./hisaab/${date}.txt`, req.body.content, (err) => {
         if (err) return res.status(500).send(err);
         res.redirect('/');
-});
+    });
 });
 app.listen(3000);
